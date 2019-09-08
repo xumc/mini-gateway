61453c9dc2614229d07e88710d335cf270fb2a4e
xumc <hmilym@gmail.com>
2019-09-08 23:15:28 +0800

    add rate limiter handler commit

--- rate_limiter.go
+++ rate_limiter.go
@@ -3,27 +3,49 @@ package main
 import (
 	"golang.org/x/time/rate"
 	"net/http"
+	"strings"
 )
+
+// TODO how to customize condtions.
+type condition struct {
+	ip string
+}
 
 type rateLimiterHandler struct {
 	next http.Handler
 
-	lim *rate.Limiter
+	// TODO lims are read-only after gateway starts, no need to lock and unlock. Afterward, if we
+	// want to support dynamic loading rate limit configuration, we need to add sync.RWlock.
+	lims map[condition]*rate.Limiter
 }
 
 func NewRateLimiterHandler(next http.Handler) http.Handler {
-	lim := rate.NewLimiter(rate.Limit(1), 1) // TODO configurable
-
 	limHandler := &rateLimiterHandler{
 		next: next,
-		lim:  lim,
+		lims: make(map[condition]*rate.Limiter),
 	}
+
+	lim := rate.NewLimiter(rate.Limit(1), 1) // TODO configurable
+	con := condition{ip: "127.0.0.1"}
+
+	limHandler.lims[con] = lim
 
 	return limHandler
 }
 
 func (r *rateLimiterHandler) ServeHTTP(resp http.ResponseWriter, req *http.Request) {
-	if r.lim.Allow() {
+	con := condition{
+		ip: strings.Split(req.RemoteAddr, ":")[0], // TODO handle special ip.
+	}
+
+	var lim *rate.Limiter
+	var ok bool
+	if lim, ok = r.lims[con]; !ok {
+		r.next.ServeHTTP(resp, req)
+		return
+	}
+
+	if lim.Allow() {
 		r.next.ServeHTTP(resp, req)
 	} else {
 		resp.WriteHeader(429)
